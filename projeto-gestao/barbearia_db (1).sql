33e6fec755c53a5f452018d82ae58ab0befacd0e
Nicolas Villela <105022587+nicolasanalyst@users.noreply.github.com>
2025-11-07 21:34:22 -0300

    Update barbearia_db (1).sql

--- projeto-gestao/barbearia_db (1).sql
+++ projeto-gestao/barbearia_db (1).sql
@@ -1,31 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 02/10/2025 às 05:41
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `barbearia_db`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `agendamentos`
---
 
 CREATE TABLE `agendamentos` (
   `id` int(11) NOT NULL,
@@ -37,10 +12,6 @@ CREATE TABLE `agendamentos` (
   `status` varchar(50) DEFAULT 'Agendado',
   `data_criacao` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Despejando dados para a tabela `agendamentos`
---
 
 INSERT INTO `agendamentos` (`id`, `cliente_id`, `servico`, `data_agendamento`, `hora_agendamento`, `valor`, `status`, `data_criacao`) VALUES
 (1, 3, 'Corte de Cabelo', '2025-09-22', '09:00:00', 40.00, 'Agendado', '2025-09-21 19:35:22'),
@@ -62,22 +33,12 @@ INSERT INTO `agendamentos` (`id`, `cliente_id`, `servico`, `data_agendamento`, `
 (28, 24, 'Corte de Cabelo', '2025-10-03', '09:30:00', 40.00, 'Agendado', '2025-10-02 02:05:29'),
 (29, 5, 'Corte de Cabelo', '2025-10-03', '10:00:00', 40.00, 'Agendado', '2025-10-02 03:09:34');
 
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `clientes`
---
-
 CREATE TABLE `clientes` (
   `id` int(11) NOT NULL,
   `nome` varchar(255) NOT NULL,
   `telefone` varchar(20) NOT NULL,
   `data_criacao` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Despejando dados para a tabela `clientes`
---
 
 INSERT INTO `clientes` (`id`, `nome`, `telefone`, `data_criacao`) VALUES
 (2, 'Carlos Souza', '(19) 91234-5678', '2025-09-21 19:35:21'),
@@ -96,51 +57,20 @@ INSERT INTO `clientes` (`id`, `nome`, `telefone`, `data_criacao`) VALUES
 (23, 'Daniel', '77777777777', '2025-10-01 23:05:37'),
 (24, 'Nicolas', '1993339597', '2025-10-02 02:05:29');
 
---
--- Índices para tabelas despejadas
---
-
---
--- Índices de tabela `agendamentos`
---
 ALTER TABLE `agendamentos`
   ADD PRIMARY KEY (`id`),
   ADD KEY `cliente_id` (`cliente_id`);
 
---
--- Índices de tabela `clientes`
---
 ALTER TABLE `clientes`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `telefone` (`telefone`);
 
---
--- AUTO_INCREMENT para tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `agendamentos`
---
 ALTER TABLE `agendamentos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
---
--- AUTO_INCREMENT de tabela `clientes`
---
 ALTER TABLE `clientes`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
---
--- Restrições para tabelas despejadas
---
-
---
--- Restrições para tabelas `agendamentos`
---
 ALTER TABLE `agendamentos`
   ADD CONSTRAINT `agendamentos_ibfk_1` FOREIGN KEY (`cliente_id`) REFERENCES `clientes` (`id`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
